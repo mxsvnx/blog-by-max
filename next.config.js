36aa91d10d236da8d415091633e2626742f83832
Max Osmanov <kenk0899@yandex.com>
2025-07-25 19:53:05 +0500

    fixed images

--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
-    ],
+    domains: ['localhost', 'blog-by-max.vercel.app'], // 👈 Явно добавили допустимые хосты
   },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
